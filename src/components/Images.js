7e3d6bb27af3dd3f4dcc89eda6470cfec6d9d13f
nsafarova <nigarsafarova@hotmail.com>
2023-05-04 22:27:39 +0400

    update jumbotron

--- src/components/Images.js
+++ src/components/Images.js
@@ -8,13 +8,13 @@ const Images = () => {
   const {response, isLoading, searchImage} = useContext(ImageContext);
 
   return (
-    <>
+    <div className="px-4 md:px-0">
       <h1 className="mt-8 font-bold text-2xl max-w-7xl mx-auto">Results for <span className="capitalize">{searchImage || 'Cats'}</span></h1>
 
       <div className="columns-2 md:columns-3 lg:columns-4 gap-6 my-8 max-w-7xl mx-auto">
         {isLoading ? <Skeleton item={10} /> : response.map((data, key) => <Image key={key} data={data}/>)}
       </div>
-    </>
+    </div>
   );
 };
 export default Images;
--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Jumbotron = ({ children }) => {
   return (
-    <div className="bg-gradient-to-tr from-emerald-400 via-purple-400 to-cyan-100 flex items-center py-9 px-10 md:px-4">
+    <div className="bg-gradient-to-tr from-emerald-400 via-purple-400 to-cyan-100 flex items-center py-9 px-4">
       <div className="max-w-xl mx-auto w-full">
         <h1 className="text-3xl font-bold text-white mb-5">Find Images</h1>
         <p className="text-md text-white ">
